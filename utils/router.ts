9f88b78e7df8b2abb70d7a95537ed5babca0ca80
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2024-06-05 16:27:15 +0100

    ALL GLORY TO GOD

---
+++ utils/router.ts
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../components/fixed/MainLayout";
+
+export const routes = createBrowserRouter([
+  {
+    path: '/'
+  }
+])
