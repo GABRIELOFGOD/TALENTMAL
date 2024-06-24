f721f7bfb6942e7a54c7a6612232b8ed2bb361e5
Ada-nna <cyndigal2@gmail.com>
2024-06-24 11:54:36 +0100

    Corrections made on the homepage small screen

--- src/components/EvaluationCards.tsx
+++ src/components/EvaluationCards.tsx
@@ -17,7 +17,7 @@ const EvaluationCards: React.FC<CardProps> = ({
     <section className="max-w-[37rem] bg-[var(--talentmal-color-seven)] lg:pt-10 lg:pl-7 p-4 lg:rounded-tl-[32px] rounded-tl-[14px]">
       <img src={icon} alt="" className="lg:w-[42px] w-[19px]" />
       <h2 className="text-[20px] font-medium lg:text-[28px] leading-[130%] lg:-tracking-[1px] -tracking-[0.46px] my-2">{title}</h2>
-      <p className="text-[9px] lgtext-[18px] text-[var(--talentmal-color-one)] leading:[12px] lg:leading-[140%] -tracking-[1px] lg:-tracking-[0.46px] mb-3 pr-5">
+      <p className="text-[12px] lgtext-[18px] text-[var(--talentmal-color-one)] leading:[12px] lg:leading-[140%] lg:-tracking-[0.46px] mb-3 pr-5">
         {detail}
       </p>
       <img src={image} alt="image" />
--- src/components/OfferCards.tsx
+++ src/components/OfferCards.tsx
@@ -12,7 +12,7 @@ const OfferCards: React.FC<CardProps> = ({ icon, title, detail }) => {
       <div className="lg:max-w-[31.1rem] border border-[#D7D7D7] bg-[var(--talentmal-color-six)] rounded-[22px] p-5">
         <img src={icon} alt="" className="lg:w-[42px] w-[27px]" />
         <h1 className="text-[20px] lg:text-[32px] font-medium lg:font-black lg:-tracking-[1px] my-3">{title}</h1>
-        <p className="text-[13px] lg:text-[20px] leading-[130%] -tracking-[0.65px] lg:-tracking-[1px] text-[var(--talentmal-color-one)]">
+        <p className="text-[14px] lg:text-[20px] leading-[130%]  text-[var(--talentmal-color-one)]">
           {detail}
         </p>
       </div>
